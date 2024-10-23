afb07bd3c477afadc8a4b757b7a4c76d713a0239
BAQAR RAZA MANGRANI <baqarmangrani@gmail.com>
2024-10-22 23:38:32 -0400

    adding logging in Companies controller

--- WebApi/App_Start/NinjectWebCommon.cs
+++ WebApi/App_Start/NinjectWebCommon.cs
@@ -3,6 +3,7 @@ using BusinessLayer;
 using DataAccessLayer.Database;
 using DataAccessLayer.Model.Interfaces;
 using DataAccessLayer.Repositories;
+using Serilog;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(WebApi.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(WebApi.App_Start.NinjectWebCommon), "Stop")]
@@ -71,6 +72,15 @@ namespace WebApi.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // Register Serilog with Ninject
+            kernel.Bind<ILogger>().ToMethod(context => Log.Logger).InSingletonScope();
+
             kernel.Bind<IMapper>().ToMethod(context =>
             {
                 var config = new MapperConfiguration(cfg =>
--- WebApi/Controllers/CompaniesController.cs
+++ WebApi/Controllers/CompaniesController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BusinessLayer.Model.Interfaces;
 using BusinessLayer.Model.Models;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,33 +16,51 @@ namespace WebApi.Controllers
     {
         private readonly ICompanyService _companyService;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
 
-        public CompaniesController(ICompanyService companyService, IMapper mapper)
+        public CompaniesController(ICompanyService companyService, IMapper mapper, ILogger logger)
         {
             _companyService = companyService;
             _mapper = mapper;
+            _logger = logger;
         }
 
         // GET api/companies
         [HttpGet]
         public async Task<IHttpActionResult> GetAll()
         {
-            var items = await _companyService.GetAllCompaniesAsync();
-            var companyDtos = _mapper.Map<IEnumerable<CompanyDto>>(items);
-            return Ok(companyDtos);
+            try
+            {
+                var items = await _companyService.GetAllCompaniesAsync();
+                var companyDtos = _mapper.Map<IEnumerable<CompanyDto>>(items);
+                return Ok(companyDtos);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error occurred while getting all companies.");
+                return InternalServerError(new Exception("An error occurred while processing your request."));
+            }
         }
 
         // GET api/companies/{companyCode}
         [HttpGet, Route("{companyCode}", Name = "GetCompanyByCode")]
         public async Task<IHttpActionResult> Get(string companyCode)
         {
-            var item = await _companyService.GetCompanyByCodeAsync(companyCode);
-            if (item == null)
+            try
             {
-                return NotFound();
+                var item = await _companyService.GetCompanyByCodeAsync(companyCode);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                var companyDto = _mapper.Map<CompanyDto>(item);
+                return Ok(companyDto);
             }
-            var companyDto = _mapper.Map<CompanyDto>(item);
-            return Ok(companyDto);
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Error occurred while getting company with code {companyCode}.");
+                return InternalServerError(new Exception("An error occurred while processing your request."));
+            }
         }
 
         // POST api/companies
@@ -52,15 +72,23 @@ namespace WebApi.Controllers
                 return BadRequest("Request is null");
             }
 
-            var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
-            var result = await _companyService.AddCompanyAsync(companyInfo);
-
-            if (!result.Success)
+            try
             {
-                return Content(HttpStatusCode.Conflict, result.Message);
-            }
+                var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
+                var result = await _companyService.AddCompanyAsync(companyInfo);
 
-            return CreatedAtRoute("GetCompanyByCode", new { companyCode = companyInfo.CompanyCode }, companyDto);
+                if (!result.Success)
+                {
+                    return Content(HttpStatusCode.Conflict, result.Message);
+                }
+
+                return CreatedAtRoute("GetCompanyByCode", new { companyCode = companyInfo.CompanyCode }, companyDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error occurred while adding a new company.");
+                return InternalServerError(new Exception("An error occurred while processing your request."));
+            }
         }
 
         // PUT api/companies/{companyCode}
@@ -72,25 +100,41 @@ namespace WebApi.Controllers
                 return BadRequest("Request is null");
             }
 
-            var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
-            var result = await _companyService.UpdateCompanyByCodeAsync(companyCode, companyInfo);
-            if (!result)
+            try
             {
-                return NotFound();
+                var companyInfo = _mapper.Map<CompanyInfo>(companyDto);
+                var result = await _companyService.UpdateCompanyByCodeAsync(companyCode, companyInfo);
+                if (!result)
+                {
+                    return NotFound();
+                }
+                return StatusCode(HttpStatusCode.NoContent);
             }
-            return StatusCode(HttpStatusCode.NoContent);
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Error occurred while updating company with code {companyCode}.");
+                return InternalServerError(new Exception("An error occurred while processing your request."));
+            }
         }
 
         // DELETE api/companies/{companyCode}
         [HttpDelete, Route("{companyCode}")]
         public async Task<IHttpActionResult> Delete(string companyCode)
         {
-            var result = await _companyService.DeleteCompanyByCodeAsync(companyCode);
-            if (!result)
+            try
             {
-                return NotFound();
+                var result = await _companyService.DeleteCompanyByCodeAsync(companyCode);
+                if (!result)
+                {
+                    return NotFound();
+                }
+                return StatusCode(HttpStatusCode.NoContent);
             }
-            return StatusCode(HttpStatusCode.NoContent);
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Error occurred while deleting company with code {companyCode}.");
+                return InternalServerError(new Exception("An error occurred while processing your request."));
+            }
         }
     }
 }
